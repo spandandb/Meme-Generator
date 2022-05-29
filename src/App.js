0cf8e5063bc2315c3b4bfd3b47ad89f466beb601
Spandan Das Barman <spandandb94@gmail.com>
2022-05-29 16:16:28 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import Form from './components/Form';
 import React, { useState } from 'react';
+import Footer from './components/Footer';
 
 function App() {
   const [lightMode, setLightMode] = useState(true);
@@ -11,6 +12,7 @@ function App() {
     <div className={lightMode ? '' : 'dark'}>
       <Navbar lightMode={lightMode} modeToggler={modeToggler} />
       <Form />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className='bg-[#1E2021] text-center p-10 dark:bg-[#3B3B3B]'>
+      <h6 className='text-transparent text-lg bg-clip-text bg-gradient-to-r from-indigo-200  to-pink-500'>
+        {' '}
+        © 2022 : Created by 🔥 Spandan Das Barman | All rights reserved{' '}
+      </h6>
+    </div>
+  );
+}
